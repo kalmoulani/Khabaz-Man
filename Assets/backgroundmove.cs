54b8fb7ca4f654deef7fef65f5cd46e84b143694
kalmoulani <kalmoulani@gmail.com>
2016-11-01 20:21:36 +0300

    the whole test game
    a moving background unity game

---
+++ Assets/backgroundmove.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class backgroundmove : MonoBehaviour {
+
+    public float scrollSpeed = 0.5F;
+    public Renderer rend;
+    // Use this for initialization
+    void Start () {
+        rend = GetComponent<Renderer>();
+    }
+	
+	// Update is called once per frame
+	void Update () {
+        float offset = Time.time * scrollSpeed;
+        rend.material.SetTextureOffset("_MainTex", new Vector2(offset, 0));
+    }
+}
